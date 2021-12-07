f227d56cd19b7d914a661bf6c9335067aba2e3a2
Deepak0844 <deepakram0105@gmail.com>
2021-12-07 17:12:24 +0530

    testing

---
+++ helper.js
@@ -0,0 +1,51 @@
+import { client } from "./index.js";
+import { ObjectId } from "mongodb";
+
+async function pizzaList() {
+    return await client.db("b28wd").collection("pizza").find().toArray();
+  }
+  
+  async function addPizza(data) {
+    return await client.db("b28wd").collection("pizza").insertMany(data);
+  }
+  
+  async function addCart(cartItem) {
+    return await client.db("b28wd").collection("cart").insertOne(cartItem);
+  }
+  
+  async function cartList() {
+    return await client.db("b28wd").collection("cart").find().toArray();
+  }
+  
+  async function getPizzaById(id) {
+    return await client
+      .db("b28wd")
+      .collection("pizza")
+      .findOne({ _id: ObjectId(id) });
+  }
+  
+  async function deleteItem(id) {
+    return await client
+      .db("b28wd")
+      .collection("cart")
+      .deleteOne({ _id: ObjectId(id) });
+  }
+  
+  async function checkOut(details) {
+    return await client.db("b28wd").collection("checkout").insertOne(details);
+  }
+  
+  async function succesful() {
+    return await client.db("b28wd").collection("checkout").findOne();
+  }
+  export {
+    pizzaList,
+    addPizza,
+    addCart,
+    cartList,
+    getPizzaById,
+    deleteItem,
+    checkOut,
+    succesful,
+  };
+  
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-// import { authRouter} from './routes/authRoutes.js';
-// import { pizzaApp} from './routes/pizzaApp.js';
+import {
+  pizzaList,
+  addPizza,
+  addCart,
+  cartList,
+  getPizzaById,
+  deleteItem,
+  checkOut,
+  succesful,
+} from "./Helper.js";
 import {MongoClient} from "mongodb";
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -12,6 +20,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL
+
+
 const PORT = process.env.PORT
   async function createConnections(){
   const client = new MongoClient(MONGO_URL);
@@ -25,7 +35,19 @@ export const client = await createConnections();
 
 app.get("/home",async(request, response) => {
     response.send("home page");
-  })
-// app.use("/user",authRouter);//middleware
+})
+
+app.get("/pizza",async(request, response) => {
+    const result = await pizzaList();
+     response.send(result);
+})
+
+app.post("/pizza",async(request, response) => {
+    const data = request.body;
+        const result = await addPizza(data);
+        response.send(result);
+})
+
+
 
 app.listen(PORT,()=>{ console.log("Server started", PORT) })
